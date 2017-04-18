8c0260763fbd372d8cd09bd894f10ceefa9d9f0a
Alex <alphanumeric@openmailbox.org>
2017-04-18 16:06:12 -0700

    hard coding some vars

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.MONGODB_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  appId: process.env.APP_ID || 'dispatchrr',
+  masterKey: process.env.MASTER_KEY || '6gz68iIA8h', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL  // Don't forget to change to https if needed
+//   liveQuery: {
+//     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+//   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -31,7 +31,7 @@ var app = express();
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+var mountPath = process.env.PARSE_MOUNT || '/1';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
@@ -52,4 +52,4 @@ httpServer.listen(port, function() {
 });
 
 // This will enable the Live Query real-time server
-ParseServer.createLiveQueryServer(httpServer);
+//ParseServer.createLiveQueryServer(httpServer);
